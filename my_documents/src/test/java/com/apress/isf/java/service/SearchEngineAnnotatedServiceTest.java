aff89e3f0ca0395ce0c212cae3809784bfe09d70
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2017-06-04 16:58:49 +0200

    Complete configuration about annotations and add test class

---
+++ my_documents/src/test/java/com/apress/isf/java/service/SearchEngineAnnotatedServiceTest.java
@@ -0,0 +1,44 @@
+package com.apress.isf.java.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.apress.isf.java.model.Document;
+import com.apress.isf.java.model.Type;
+
+public class SearchEngineAnnotatedServiceTest {
+
+	private ClassPathXmlApplicationContext context;
+	private Type webType;
+	private SearchEngine searchEngine = new MySearchEngine();
+	
+	@Before
+	public void setup() {
+		context = new ClassPathXmlApplicationContext("META-INF/spring/mydocuments-annotations-context.xml");
+		searchEngine = context.getBean("searchEngine", SearchEngine.class);
+		webType = context.getBean("webType", Type.class);
+	}
+
+	@Test
+	public void testFindByType() {
+		
+		List<Document> documents = searchEngine.findByType(webType);
+		assertThat(documents.size(), is(1));
+		assertThat(documents.get(0).getType().getName(), is(webType.getName()));
+		assertThat(documents.get(0).getType().getDesc(), is(webType.getDesc()));
+		assertThat(documents.get(0).getType().getExtension(), is(webType.getExtension()));
+	}
+
+	@Test
+	public void testListAll() {
+		List<Document> documents = searchEngine.listAll();
+		assertThat(documents.size(), is(4));
+	}
+
+}
